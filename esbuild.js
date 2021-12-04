c343539fda24ae173b5c0d2997169b88ff09b430
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-12-04 17:43:27 +0700

    chore: add esbuild file

---
+++ esbuild.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function getFiles(dir, files_) {
+    files_ = files_ || [];
+    let files = fs.readdirSync(dir);
+    for (let i in files) {
+        let name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+require("esbuild")
+    .buildSync({
+        entryPoints: getFiles('src'),
+        bundle: false,
+        platform: "node",
+        format: "cjs",
+        outdir: "dist",
+    })
