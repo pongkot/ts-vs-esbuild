44fddbe31f4f3b2389478d0e8804ca210e4c182b
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-12-04 20:30:46 +0700

    chore: add build script

---
+++ build.js
@@ -0,0 +1,20 @@
+const {exec} = require('child_process');
+const args = process.argv
+const execute = cmd => {
+    const label = cmd
+    console.time(label)
+    exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+        console.timeEnd(label)
+    });
+}
+const isIn = (item, list = args) => list.indexOf(item) !== -1
+const isTypescript = isIn('--typescript')
+const isESBuild = isIn('--esbuild')
+
+if (isTypescript) execute('tsc')
+if (isESBuild) execute('node esbuild')
